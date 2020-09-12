01aa3001fa427acd3e20af607e8891b052dd8568
Julio <julio.olvr@gmail.com>
2020-09-12 12:01:20 -0300

    Cleanup build and tests

--- test/main.test.tsx
+++ test/main.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Default as Thing } from '../stories/Thing.stories';
+import { Default as HorizontalCollapse } from '../stories/HorizontalCollapse.stories';
 
 describe('Thing', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<HorizontalCollapse />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
